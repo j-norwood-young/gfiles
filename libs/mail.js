0f1307f8cdfa205e9b4b6c02fd74442b042543b3
Jason Norwood-Young <jason@10layer.com>
2017-09-17 10:54:45 +0200

    Add user

---
+++ libs/mail.js
@@ -0,0 +1,31 @@
+var config = require('config');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var transporter = nodemailer.createTransport(smtpTransport(config.smtp));
+
+var send = function(mailObj) {
+	return new Promise((resolve, reject) => {
+		transporter.sendMail(mailObj, function(err, info) {
+			if (err) {
+				console.error(err);
+				logger.error(null, "Failed to send email", null, { mailObj, err });
+				return reject(err);
+			}
+			return resolve(info);
+		});
+	});
+};
+
+var mail = {
+	send: function(params) {
+		var mailObj = {
+			from: params.from || config.smtp_from,
+			to: params.to,
+			subject: params.subject,
+			html: params.body
+		};
+		return send(mailObj);
+	},
+};
+
+module.exports = mail;
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var mongo = require("../libs/mongo");
 var async = require("async");
 var execFile = require("child_process").execFile;
+var generator = require('generate-password');
+var mail = require("../libs/mail");
 
 router.use(mongo.user.restricted);
 
@@ -172,4 +174,55 @@ router.get("/index/xlsx", (req, res, next) => {
 		res.send(err)
 	});
 });
+
+router.get("/users/add", (req, res, next) => {
+	res.render("users/add");
+});
+
+router.post("/users/add", (req, res) => {
+	var email = req.body.email;
+	var name = req.body.name;
+	var password = generator.generate({
+		length: 12,
+		numbers: true
+	});
+	// res.send({ name, email, password });
+	req.apihelper.post("user", { email, name, password })
+	.then(result => {
+		mail.send({
+			to: email,
+			from: "jason@10layer.com",
+			subject: "Welcome to GuptaFiles",
+			body: `Hi ${name},<br>
+<br>
+Welcome to the GuptaFiles, an online resource for exploring and interviewing the files associated to the Gupta Leaks.<br>
+<br>
+You can log in by going to <a href="https://guptafiles.sourcery.info">https://guptafiles.sourcery.info</a> and logging in with the following information:<br>
+Email: ${ email }<br>
+Password: ${ password }<br>
+<br>
+Please do not share your password with anyone. If you need to give someone else access, please contact Jason at jason@10layer.com.<br>
+<br>
+All of the Gupta files have been inspected for their metadata, which in some cases include author, editor, whether (and when) a file was printed, and even GPS data.<br>
+<br>
+Many of the files (particularly PDFs, XLSX, Doc and Docx) have had the text extracted, and that is searchable in the database.<br>
+<br>
+Full-text search tips:<br>
+If you use a number of words, the system will do an OR search, eg <em>gupta bribe</em> would search gupta OR bribe.<br>
+Use double quotes to search for a term, eg. <em>"Search this whole term"</em>.<br>
+Use a minus sign to exclude a word, eg. <em>gupta -bribe</em> to exclude the word bribe.<br>
+<br>
+More features are planned, so check back often. We will note new features on the first page.<br>
+<br>
+Happy hunting!<br>
+Jason`
+		})
+		res.render("users/add", { alert: { msg: "User added", status: "success" } } );
+	})
+	.catch(err => {
+		console.error(err);
+		res.render("users/add", { alert: { msg: err.message, status: "danger" } } );
+	});
+});
+
 module.exports = router;
